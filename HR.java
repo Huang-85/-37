1013b1bcacd14fb5489d34f8ba0dbab6725fa99e
tangyanyu39 <82568437+tangyanyu39@users.noreply.github.com>
2021-05-06 15:23:45 +0800

    Update HR.java

--- HR.java
+++ HR.java
@@ -5,7 +5,8 @@ public class HR {
 	public static void main(String[] args) {
 		
 		System.out.println("hello world!");
-		System.out.println("�ϴ�  hello world!");
+		System.out.println("ÉÏ´«  hello world!");
+		System.out.println("hello  new  world");
 	}
 
 }
